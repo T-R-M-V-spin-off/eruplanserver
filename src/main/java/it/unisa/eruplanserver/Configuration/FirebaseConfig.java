6d7cb84e8090ee7865f8c1e5a930b4096d01c277
angeloantonioprisco <a.prisco50@studenti.unisa.it>
2025-12-03 19:27:10 +0100

    Changed FirebaseConfig.java to use environment variable instead of static file because with the file can't be pushed and Azure can't build the application without the file.

--- src/main/java/it/unisa/eruplanserver/Configuration/FirebaseConfig.java
+++ src/main/java/it/unisa/eruplanserver/Configuration/FirebaseConfig.java
@@ -3,28 +3,35 @@ package it.unisa.eruplanserver.Configuration;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.io.ClassPathResource;
-
-import java.io.IOException;
+import java.io.ByteArrayInputStream;
 import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class FirebaseConfig {
 
+    @Value("${FIREBASE_CREDENTIALS}")
+    private String firebaseCredentialsJson;
+
     @Bean
-    public FirebaseApp firebaseApp() throws IOException {
+    public FirebaseApp firebaseApp() throws Exception {
+
         if (FirebaseApp.getApps().isEmpty()) {
-            InputStream serviceAccount = new ClassPathResource("eruplan-firebase-adminsdk.json").getInputStream();
+
+            InputStream serviceAccount = new ByteArrayInputStream(
+                    firebaseCredentialsJson.getBytes(StandardCharsets.UTF_8)
+            );
 
             FirebaseOptions options = FirebaseOptions.builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
                     .build();
 
             return FirebaseApp.initializeApp(options);
-        } else {
-            return FirebaseApp.getInstance();
         }
+
+        return FirebaseApp.getInstance();
     }
 }
