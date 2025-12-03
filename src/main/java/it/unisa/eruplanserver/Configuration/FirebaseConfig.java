f6107f713ecd5db836a5d45b4d62a1829b82cf51
angeloantonioprisco <a.prisco50@studenti.unisa.it>
2025-12-03 20:14:09 +0100

    Changed FirebaseConfig.java and pom.xml to use environment variables.

--- src/main/java/it/unisa/eruplanserver/Configuration/FirebaseConfig.java
+++ src/main/java/it/unisa/eruplanserver/Configuration/FirebaseConfig.java
@@ -3,6 +3,8 @@ package it.unisa.eruplanserver.Configuration;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,25 +15,33 @@ import java.nio.charset.StandardCharsets;
 @Configuration
 public class FirebaseConfig {
 
+    private static final Logger logger = LoggerFactory.getLogger(FirebaseConfig.class);
+
     @Value("${FIREBASE_CREDENTIALS}")
     private String firebaseCredentialsJson;
 
     @Bean
-    public FirebaseApp firebaseApp() throws Exception {
+    public FirebaseApp firebaseApp() {
+        try {
+            if (FirebaseApp.getApps().isEmpty()) {
 
-        if (FirebaseApp.getApps().isEmpty()) {
+                // Sostituisci i \n letterali con newline reali
+                String credentials = firebaseCredentialsJson.replace("\\n", "\n");
 
-            InputStream serviceAccount = new ByteArrayInputStream(
-                    firebaseCredentialsJson.getBytes(StandardCharsets.UTF_8)
-            );
+                InputStream serviceAccount = new ByteArrayInputStream(
+                        credentials.getBytes(StandardCharsets.UTF_8)
+                );
 
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
+                FirebaseOptions options = FirebaseOptions.builder()
+                        .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                        .build();
 
-            return FirebaseApp.initializeApp(options);
+                return FirebaseApp.initializeApp(options);
+            } else {
+                return FirebaseApp.getInstance();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("Impossibile inizializzare FirebaseApp", e);
         }
-
-        return FirebaseApp.getInstance();
     }
 }
