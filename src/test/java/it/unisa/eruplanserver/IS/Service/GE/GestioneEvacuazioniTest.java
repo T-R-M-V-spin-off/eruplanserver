ceef198eaa44ccf1c81c9ca809c5c96d1100fdc6
MGiordano202 <114730112+MGiordano202@users.noreply.github.com>
2025-12-11 08:48:27 +0100

    Fix: in GestioneEvacuazioniTest.java i metodi invalidLenght e invalidCharacters utilizzano il metodo di validazione isCodiceFiscaleValid di Validator.java

--- src/test/java/it/unisa/eruplanserver/IS/Service/GE/GestioneEvacuazioniTest.java
+++ src/test/java/it/unisa/eruplanserver/IS/Service/GE/GestioneEvacuazioniTest.java
@@ -1,5 +1,6 @@
-package eruplan.unisa.eruplan.utility;
+package it.unisa.eruplanserver.IS.Service.GE;
 
+import it.unisa.eruplanserver.IS.Utility.Validator;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -14,7 +15,7 @@ class GestioneEvacuazioniTest {
         // Codice fiscale con lunghezza != 16
         String cf = "GRT36TGHH53"; // 11 caratteri
 
-        assertFalse(Validator.isCodiceFiscaleLengthValid(cf),
+        assertFalse(Validator.isCodiceFiscaleValid(cf),
                 "Atteso false: lunghezza non valida");
     }
 
@@ -26,7 +27,7 @@ class GestioneEvacuazioniTest {
         // Codice fiscale lungo 16 ma contiene simboli non ammessi
         String cf = "GRT36T$%GHH5334G";
 
-        assertFalse(Validator.isCodiceFiscaleCharactersValid(cf),
+        assertFalse(Validator.isCodiceFiscaleValid(cf),
                 "Atteso false: caratteri non validi");
     }
 }
